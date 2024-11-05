61c83170dbf895b2162a15d464fd4cda90917c8e
jorgluiz <jorgeluizti@hotmail.com>
2024-11-05 16:41:03 -0300

    chore: HomePage for home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Layout from '../components/layout/Layout'
 
-const HomePage: React.FC = () => {
+const Home: React.FC = () => {
   return <Layout></Layout>
 }
 
-export default HomePage
+export default Home
